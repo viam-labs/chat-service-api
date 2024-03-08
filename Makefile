d42cd39158cced0327f678cf24e7428453a9d404
Matt Vella <mcvella@gmail.com>
2024-03-08 18:09:28 -0500

    Add TS

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 generate:
-	buf generate --template ./src/proto/buf.gen.yaml ./src/proto -o ./src
+	export PATH="${PATH}:${HOME}/go/bin";buf generate buf.build/googleapis/googleapis --template ./src/proto/buf.gen.yaml  -o ./src
+	export PATH="${PATH}:${HOME}/go/bin";buf generate --template ./src/proto/buf.gen.yaml ./src/proto -o ./src
 	sed -i "" -e "s/import\ chat_pb2/from\ \.\ import\ chat_pb2/g" ./src/chat_service_api/grpc/chat_grpc.py
---
+++ src/chat_service_api_js/grpc/chat_pb.d.ts
@@ -0,0 +1,50 @@
+// package: viamlabs.service.chat.v1
+// file: chat.proto
+
+import * as jspb from "google-protobuf";
+import * as google_api_annotations_pb from "./google/api/annotations_pb";
+
+export class ChatRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  getMessage(): string;
+  setMessage(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ChatRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: ChatRequest): ChatRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ChatRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ChatRequest;
+  static deserializeBinaryFromReader(message: ChatRequest, reader: jspb.BinaryReader): ChatRequest;
+}
+
+export namespace ChatRequest {
+  export type AsObject = {
+    name: string,
+    message: string,
+  }
+}
+
+export class ChatResponse extends jspb.Message {
+  getAnswer(): string;
+  setAnswer(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ChatResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: ChatResponse): ChatResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ChatResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ChatResponse;
+  static deserializeBinaryFromReader(message: ChatResponse, reader: jspb.BinaryReader): ChatResponse;
+}
+
+export namespace ChatResponse {
+  export type AsObject = {
+    answer: string,
+  }
+}
+
